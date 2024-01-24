46b1cad0631bb4ce3261edaed65f786a4826574c
Pratik Singh Rawat <pratikrawat27@gmail.com>
2024-01-24 20:07:18 +0530

    learn about rest operator

---
+++ basics_3/2.FunctionsWithObjectsAndArray.js
@@ -0,0 +1,7 @@
+// If you dont know how much argument you will get eg shopping will can add multiple unlimited product then get the total amt
+
+function calCartPrice(...num){ // same as spread it depend on the usage of the operator.
+    return num;
+}
+
+console.log(calCartPrice(13,41,53,53)); // return a array
