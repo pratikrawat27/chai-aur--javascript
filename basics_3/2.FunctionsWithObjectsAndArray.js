8355286f168c77f71a56dd9fa75008f481cb79e4
Pratik Singh Rawat <pratikrawat27@gmail.com>
2024-01-25 11:44:16 +0530

    learn about how to pass obj and array in funcation

--- basics_3/2.FunctionsWithObjectsAndArray.js
+++ basics_3/2.FunctionsWithObjectsAndArray.js
@@ -5,3 +5,31 @@ function calCartPrice(...num){ // same as spread it depend on the usage of the o
 }
 
 console.log(calCartPrice(13,41,53,53)); // return a array
+
+// Passing object in function
+
+const user = {
+    name:"Pratik",
+    age:20,
+}
+
+function handleObj(anyObj){
+    return `Hello my name is ${anyObj.name} and I'm ${anyObj.age} year old.`;
+}
+
+console.log(handleObj(user));
+console.log(handleObj({
+    name: "Sonu",
+    age: 26
+})); // another way to pass object in function
+
+// Passing Array in function
+
+const myarray = [12,14,16];
+
+function returnArray(array){
+    return `This is the second value of an array: ${array[1]}`
+}
+
+console.log(returnArray(myarray));
+console.log(returnArray([13,15,17])); // another way to pass array in function
